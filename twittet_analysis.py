eedc85166079d17938e863fdc5d83071fbe8fe66
nizam <arab.nizam@gmail.com>
2020-06-13 23:27:12 +0530

    first commit

---
+++ twittet_analysis.py
@@ -0,0 +1,18 @@
+import re    # for regular expressions 
+import nltk  # for text manipulation 
+import string 
+import warnings 
+import numpy as np 
+import pandas as pd 
+import seaborn as sns 
+import matplotlib.pyplot as plt  
+
+pd.set_option("display.max_colwidth", 200) 
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+
+%matplotlib inline
+
+train  = pd.read_csv('train_Twitter Analysis.csv') 
+train
+test = pd.read_csv('test_Twitter Analysis.csv')
+test
